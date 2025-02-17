0aefbf238a68c140841342a549586ccabff32326
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-18 00:42:31 +1000

    prepare for development

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,37 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js"
+import pluginImport from "eslint-plugin-import"
+import pluginReactHooks from "eslint-plugin-react-hooks"
+import pluginReactRefresh from "eslint-plugin-react-refresh"
+import globals from "globals"
+import tseslint from "typescript-eslint"
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+	{ ignores: ["dist"] },
+	{
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		files: ["**/*.{ts,tsx}"],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			"react-hooks": pluginReactHooks,
+			"react-refresh": pluginReactRefresh,
+		},
+		rules: {
+			semi: ["error", "never"],
+			"@typescript-eslint/no-unused-vars": "off",
+			...pluginReactHooks.configs.recommended.rules,
+			"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+		},
+	},
+	{
+		files: ["src**/*.{ts,tsx}"],
+		plugins: {
+			import: pluginImport,
+		},
+		rules: {
+			"import/no-default-export": "error",
+		},
+	}
 )
