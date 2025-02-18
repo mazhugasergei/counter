ce7915f193f38bb053e193c5203d473c3a12bca2
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-19 00:01:30 +1000

    add handler

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { cn } from "./utils/helpers"
 
 export function App() {
 	const containerRef = React.useRef<HTMLDivElement>(null)
+	const sliderRef = React.useRef<HTMLDivElement>(null)
+	const handlerRef = React.useRef<HTMLDivElement>(null)
+	const [sliderHeight, setSliderHeight] = React.useState<number | null>(null)
 	const [selectedValueIndex, setSelectedValueIndex] = React.useState(0)
 	const minValue = 0
 	const maxValue = 100
 	const step = 1
 	const majorDivision = 5
-	const radius = 10
 	const numbers = true
+	const sigma = 5
 
 	if (maxValue <= minValue) return <Block>Max value must be greater than min value</Block>
 	if (!step) return <Block>Step must be defined and greater than 0</Block>
@@ -22,27 +25,46 @@ export function App() {
 	}, [minValue, maxValue, step])
 
 	React.useEffect(() => {
-		if (!containerRef.current) return
+		if (!sliderRef.current) return
+		setSliderHeight(sliderRef.current.offsetHeight)
+	}, [sliderRef])
 
-		const handleChangeValue = (e: WheelEvent) => {
-			setSelectedValueIndex((prev) => {
-				const newValue = prev + (e.deltaY < 0 ? 1 : -1)
-				return Math.min(Math.max(newValue, 0), values.length - 1)
-			})
+	React.useEffect(() => {
+		if (!handlerRef.current) return
+
+		let isMouseDown = false
+		const handleMouseDown = () => (isMouseDown = true)
+		const handleMouseUp = () => (isMouseDown = false)
+
+		const handleMouseMove = (e: MouseEvent) => {
+			if (!isMouseDown || !sliderRef.current || !handlerRef.current || !sliderHeight) return
+			// calculate new value
+			const offsetFromBottom = sliderRef.current.getBoundingClientRect().bottom - e.clientY
+			let newValueIndex = Math.round((offsetFromBottom / sliderHeight) * ((maxValue - minValue) / step))
+			if (newValueIndex < 0) newValueIndex = 0
+			if (newValueIndex > values.length - 1) newValueIndex = values.length - 1
+			setSelectedValueIndex(newValueIndex)
+			// move handler
+			handlerRef.current.style.top = `${sliderHeight - (sliderHeight / (values.length - 1)) * newValueIndex}px`
 		}
-		containerRef.current?.addEventListener("wheel", handleChangeValue)
+
+		handlerRef.current.addEventListener("mousedown", handleMouseDown)
+		document.addEventListener("mouseup", handleMouseUp)
+		document.addEventListener("mousemove", handleMouseMove)
 
 		return () => {
-			containerRef.current?.removeEventListener("wheel", handleChangeValue)
+			handlerRef.current?.removeEventListener("mousedown", handleMouseDown)
+			document.removeEventListener("mouseup", handleMouseUp)
+			document.removeEventListener("mousemove", handleMouseMove)
 		}
-	}, [containerRef])
+	}, [handlerRef, sliderRef, sliderHeight])
 
 	return (
 		<main>
 			{/* container */}
 			<div ref={containerRef} className="flex min-h-screen justify-between p-6">
 				{/* slider */}
-				<div className={cn("flex gap-2", numbers ? "pl-8" : "pl-2")}>
+				<div ref={sliderRef} className={cn("flex gap-2", numbers ? "pl-8" : "pl-2")}>
 					{/* dividers */}
 					<div className="flex flex-col items-end justify-between">
 						{values
@@ -53,7 +75,6 @@ export function App() {
 										width: !(index % majorDivision) ? "10px" : "7px",
 										borderTop: "1px solid var(--color-foreground)",
 										transform: (() => {
-											const sigma = radius / 2
 											const x = Math.abs(selectedValueIndex - index)
 											const y = Math.exp(-(x ** 2 / (2 * sigma ** 2)))
 											const maxOffset = 15
@@ -63,7 +84,7 @@ export function App() {
 								>
 									{/* numbers */}
 									{numbers && (
-										<div className="absolute top-0 right-[100%] -translate-x-2 -translate-y-1/2 font-mono text-xs">
+										<div className="absolute top-0 right-[100%] -translate-x-2 -translate-y-1/2 font-mono text-xs select-none">
 											{!(index % majorDivision) ? value : ""}
 										</div>
 									)}
@@ -83,7 +104,6 @@ export function App() {
 										borderTop: "1px solid var(--color-foreground)",
 										opacity: selectedValueIndex >= index ? 1 : 0.2,
 										transform: (() => {
-											const sigma = radius / 2
 											const x = Math.abs(selectedValueIndex - index)
 											const y = Math.exp(-(x ** 2 / (2 * sigma ** 2)))
 											const maxOffset = 15
@@ -94,18 +114,38 @@ export function App() {
 							))
 							.reverse()}
 					</div>
+
+					{/* handler */}
+					<div
+						ref={handlerRef}
+						className="bg-foreground relative h-4 w-4 -translate-y-[calc(50%+1px)] rounded-full"
+						style={{
+							top: "100%",
+						}}
+					/>
 				</div>
 
-				<div className="flex flex-col items-end gap-2 self-end">
-					<div className="text-secondary text-md font-mono">Value</div>
-					<div className="font-mono text-7xl">{values[selectedValueIndex]}</div>
+				<div className="flex flex-col justify-between">
+					{/* debug */}
+					<Block>
+						<pre>
+							{JSON.stringify(
+								{
+									value: values[selectedValueIndex],
+									selectedValueIndex,
+								},
+								null,
+								2
+							)}
+						</pre>
+					</Block>
+
+					<div className="flex flex-col items-end">
+						<div className="text-secondary text-md font-mono">Value</div>
+						<div className="font-mono text-7xl">{values[selectedValueIndex]}</div>
+					</div>
 				</div>
 			</div>
-
-			{/* debug */}
-			{/* <Block className="mt-2">
-				<pre>{JSON.stringify({ containerHeight, selectedValueIndex, radius }, null, 2)}</pre>
-			</Block> */}
 		</main>
 	)
 }
