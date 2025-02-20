485b110f4ab45eff25f747277943e01540cf8271
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-20 17:48:14 +1000

    extract components and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Block } from "./components/block"
 import { ChevronUpDownIcon } from "./components/icons"
+import { cn } from "./utils/helpers"
 
 export function App() {
 	const containerRef = React.useRef<HTMLDivElement>(null)
@@ -123,102 +124,261 @@ export function App() {
 	}, [containerRef, isMouseDown, isTouchStart, sliderRef, sliderHeight, values.length, minValue, maxValue, step])
 
 	return (
-		<div ref={containerRef} className="flex min-h-screen justify-between px-6 py-10 select-none">
-			{/* slider */}
-			<div
+		<Container ref={containerRef}>
+			<Slider
 				ref={sliderRef}
-				className="flex gap-3"
-				style={{
-					paddingLeft:
-						getOffset(0, 0) / 16 + (numbers ? maxValue.toString().length * 0.4125 + numbersOffset : 0) + "rem",
-				}}
+				getOffset={getOffset}
+				selectedIndex={selectedIndex}
+				numbers={numbers}
+				numbersOffset={numbersOffset}
+				maxValue={maxValue}
 			>
-				{/* dividers */}
-				<div className="flex flex-col items-end justify-between">
-					{values
-						.map((value, index) => (
-							<div
-								key={value}
-								className="relative border-t"
-								style={{
-									width: !(index % majorDivision) ? "10px" : "7px",
-									borderColor: "var(--color-foreground)",
-									right: isMouseDown || isTouchStart ? getOffset(selectedIndex, index) : 0,
-								}}
-							>
-								{/* numbers */}
-								{numbers && (
-									<div
-										className="absolute top-0 right-full -translate-y-1/2 font-mono text-xs"
-										style={{ transform: `translateX(-${numbersOffset}rem)` }}
-									>
-										{!(index % numberedDivision) ? value : ""}
-									</div>
-								)}
-							</div>
-						))
-						.reverse()}
-				</div>
+				<Dividers
+					values={values}
+					selectedIndex={selectedIndex}
+					isMouseDown={isMouseDown}
+					isTouchStart={isTouchStart}
+					getOffset={getOffset}
+					majorDivision={majorDivision}
+					numberedDivision={numberedDivision}
+					numbers={numbers}
+					numbersOffset={numbersOffset}
+				/>
 
-				{/* selected range */}
-				<div className="flex flex-col justify-between">
-					{values
-						.map((value, index) => (
-							<div
-								key={value}
-								className="relative w-1 border-t"
-								style={{
-									borderColor:
-										index > selectedIndex
-											? "var(--color-secondary)"
-											: monochrome
-												? "var(--color-foreground)"
-												: "var(--color-primary)",
-									right: isMouseDown || isTouchStart ? getOffset(selectedIndex, index) : 0,
-								}}
-							/>
-						))
-						.reverse()}
-				</div>
+				<SelectedRange
+					values={values}
+					selectedIndex={selectedIndex}
+					isMouseDown={isMouseDown}
+					isTouchStart={isTouchStart}
+					monochrome={monochrome}
+					getOffset={getOffset}
+				/>
 
-				{/* handler */}
-				<div ref={handlerRef} className="relative translate-y-1/2 self-end">
-					{isMouseDown || isTouchStart ? (
-						<div className="grid h-6 w-6 place-items-center">
-							<div className="bg-foreground h-4 w-4 rounded-full" />
-						</div>
-					) : (
-						<ChevronUpDownIcon className="pointer-events-none select-none" />
-					)}
-				</div>
-			</div>
+				<Handler ref={handlerRef} isMouseDown={isMouseDown} isTouchStart={isTouchStart} />
+			</Slider>
 
-			<div className="flex flex-col justify-end">
-				{/* debug */}
-				{debug && (
-					<Block className="mb-auto">
-						<pre>
-							{JSON.stringify(
-								{
-									sliderHeight,
-									isMouseDown,
-									isTouchStart,
-									value: values[selectedIndex],
-									selectedIndex,
-								},
-								null,
-								2
-							)}
-						</pre>
-					</Block>
-				)}
+			<Display>
+				<Debug
+					debug={debug}
+					sliderHeight={sliderHeight}
+					isMouseDown={isMouseDown}
+					isTouchStart={isTouchStart}
+					selectedIndex={selectedIndex}
+					values={values}
+				/>
 
-				{/* values */}
-				<div className="text-right font-mono">
-					<div className="text-secondary text-md">Value</div>
-					<div className="text-7xl">{values[selectedIndex]}</div>
-				</div>
-			</div>
-		</div>
+				<Values values={values} selectedIndex={selectedIndex} />
+			</Display>
+		</Container>
 	)
 }
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const Container = React.forwardRef<HTMLDivElement, ContainerProps>(({ children, className, ...props }, ref) => (
+	<div ref={ref} className={cn("flex min-h-screen justify-between px-6 py-10 select-none", className)} {...props}>
+		{children}
+	</div>
+))
+Container.displayName = "Container"
+
+interface SliderProps extends React.HTMLAttributes<HTMLDivElement> {
+	getOffset: (selectedIndex: number, index: number) => number
+	selectedIndex: number
+	numbers: boolean
+	numbersOffset: number
+	maxValue: number
+}
+
+const Slider = React.forwardRef<HTMLDivElement, SliderProps>(
+	({ getOffset, selectedIndex, numbers, numbersOffset, maxValue, children, className, ...props }, ref) => (
+		<div
+			ref={ref}
+			className={cn("flex gap-3", className)}
+			style={{
+				paddingLeft: (() => {
+					const maxOffset = getOffset(0, 0) / 16
+					const characterWidth = 6.6 / 16
+					const maxValueLength = maxValue.toString().length
+					const numbersWidth = numbers ? maxValueLength * characterWidth + numbersOffset : 0
+					return maxOffset + numbersWidth + "rem"
+				})(),
+			}}
+			{...props}
+		>
+			{children}
+		</div>
+	)
+)
+Slider.displayName = "Slider"
+
+interface DividersProps extends React.HTMLAttributes<HTMLDivElement> {
+	values: number[]
+	selectedIndex: number
+	isMouseDown: boolean
+	isTouchStart: boolean
+	getOffset: (selectedIndex: number, index: number) => number
+	majorDivision: number
+	numberedDivision: number
+	numbers: boolean
+	numbersOffset: number
+}
+
+const Dividers = React.forwardRef<HTMLDivElement, DividersProps>(
+	(
+		{
+			values,
+			selectedIndex,
+			isMouseDown,
+			isTouchStart,
+			getOffset,
+			majorDivision,
+			numberedDivision,
+			numbers,
+			numbersOffset,
+			className,
+			...props
+		},
+		ref
+	) => (
+		<div ref={ref} className={cn("flex flex-col items-end justify-between", className)} {...props}>
+			{values
+				.map((value, index) => (
+					<div
+						key={value}
+						className="relative border-t"
+						style={{
+							width: !(index % majorDivision) ? "10px" : "7px",
+							borderColor: "var(--color-foreground)",
+							right: isMouseDown || isTouchStart ? getOffset(selectedIndex, index) : 0,
+						}}
+					>
+						{/* numbers */}
+						{numbers && (
+							<div
+								className="absolute top-0 right-full -translate-y-1/2 font-mono text-xs"
+								style={{ transform: `translateX(-${numbersOffset}rem)` }}
+							>
+								{!(index % numberedDivision) ? value : ""}
+							</div>
+						)}
+					</div>
+				))
+				.reverse()}
+		</div>
+	)
+)
+Dividers.displayName = "Dividers"
+
+interface SelectedRangeProps extends React.HTMLAttributes<HTMLDivElement> {
+	values: number[]
+	selectedIndex: number
+	isMouseDown: boolean
+	isTouchStart: boolean
+	monochrome: boolean
+	getOffset: (selectedIndex: number, index: number) => number
+}
+
+const SelectedRange = React.forwardRef<HTMLDivElement, SelectedRangeProps>(
+	({ values, selectedIndex, isMouseDown, isTouchStart, monochrome, getOffset, className, ...props }, ref) => (
+		<div ref={ref} className={cn("flex flex-col justify-between", className)} {...props}>
+			{values
+				.map((value, index) => (
+					<div
+						key={value}
+						className="relative w-1 border-t"
+						style={{
+							borderColor:
+								index > selectedIndex
+									? "var(--color-secondary)"
+									: monochrome
+										? "var(--color-foreground)"
+										: "var(--color-primary)",
+							right: isMouseDown || isTouchStart ? getOffset(selectedIndex, index) : 0,
+						}}
+					/>
+				))
+				.reverse()}
+		</div>
+	)
+)
+SelectedRange.displayName = "SelectedRange"
+
+interface HandlerProps extends React.HTMLAttributes<HTMLDivElement> {
+	isMouseDown: boolean
+	isTouchStart: boolean
+}
+
+const Handler = React.forwardRef<HTMLDivElement, HandlerProps>(
+	({ isMouseDown, isTouchStart, className, ...props }, ref) => {
+		const Circle = () => (
+			<div className="grid h-6 w-6 place-items-center">
+				<div className="bg-foreground h-4 w-4 rounded-full" />
+			</div>
+		)
+
+		return (
+			<div ref={ref} className={cn("relative translate-y-1/2 self-end", className)} {...props}>
+				{isMouseDown || isTouchStart ? <Circle /> : <ChevronUpDownIcon className="pointer-events-none" />}
+			</div>
+		)
+	}
+)
+Handler.displayName = "Handler"
+
+interface DisplayProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const Display = React.forwardRef<HTMLDivElement, DisplayProps>(({ children, className, ...props }, ref) => (
+	<div ref={ref} className={cn("flex flex-col justify-end", className)} {...props}>
+		{children}
+	</div>
+))
+Display.displayName = "Display"
+
+interface DebugProps extends React.HTMLAttributes<HTMLDivElement> {
+	debug: boolean
+	sliderHeight: number | null
+	isMouseDown: boolean
+	isTouchStart: boolean
+	selectedIndex: number
+	values: number[]
+}
+
+const Debug = React.forwardRef<HTMLDivElement, DebugProps>(
+	({ debug, sliderHeight, isMouseDown, isTouchStart, selectedIndex, values }, ref) => {
+		return (
+			debug && (
+				<Block ref={ref} className="mb-auto">
+					<pre>
+						{JSON.stringify(
+							{
+								sliderHeight,
+								isMouseDown,
+								isTouchStart,
+								value: values[selectedIndex],
+								selectedIndex,
+							},
+							null,
+							2
+						)}
+					</pre>
+				</Block>
+			)
+		)
+	}
+)
+Debug.displayName = "Debug"
+
+interface ValuesProps extends React.HTMLAttributes<HTMLDivElement> {
+	values: number[]
+	selectedIndex: number
+}
+
+const Values = React.forwardRef<HTMLDivElement, ValuesProps>(({ values, selectedIndex }, ref) => {
+	return (
+		<div ref={ref} className="text-right font-mono">
+			<div className="text-secondary text-md">Value</div>
+			<div className="text-7xl">{values[selectedIndex]}</div>
+		</div>
+	)
+})
