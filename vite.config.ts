81ffb4d23a3f6595ffb545e7459875318403bf8f
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-22 15:31:08 +1000

    vite config: cgange base ony in prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	base: "/counter/",
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "")
+
+	return {
+		plugins: [react(), tailwindcss()],
+		base: env.NODE_ENV === "production" ? "/counter/" : "/",
+	}
 })
