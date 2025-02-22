db60e38f46cc1b5eb9be791ba85a0871e2470807
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-02-22 16:18:53 +1000

    icons: add settings icon; size instead of width and height

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,16 +1,15 @@
 import React from "react"
 
 export interface IconProps extends React.SVGProps<SVGSVGElement> {
-	width?: number
-	height?: number
+	size?: number
 }
 
-export const ChevronUpDownIcon = React.forwardRef<SVGSVGElement, IconProps>(({ width, height, ...props }, ref) => (
+export const ChevronUpDownIcon = React.forwardRef<SVGSVGElement, IconProps>(({ size, ...props }, ref) => (
 	<svg
 		ref={ref}
 		xmlns="http://www.w3.org/2000/svg"
-		width={width ?? 24}
-		height={height ?? 24}
+		width={size ?? 24}
+		height={size ?? 24}
 		viewBox="0 0 24 24"
 		fill="none"
 		stroke="currentColor"
@@ -24,3 +23,23 @@ export const ChevronUpDownIcon = React.forwardRef<SVGSVGElement, IconProps>(({ w
 	</svg>
 ))
 ChevronUpDownIcon.displayName = "ChevronUpDownIcon"
+
+export const SettingsIcon = React.forwardRef<SVGSVGElement, IconProps>(({ size, ...props }, ref) => (
+	<svg
+		ref={ref}
+		xmlns="http://www.w3.org/2000/svg"
+		width={size ?? 24}
+		height={size ?? 24}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth="2"
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		{...props}
+	>
+		<path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
+		<circle cx="12" cy="12" r="3" />
+	</svg>
+))
+SettingsIcon.displayName = "SettingsIcon"
